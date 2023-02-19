ea7258e640df5d51788d9725c6cdb2f952d73b20
Matvey <matvey@aytm.com>
2023-02-19 21:57:51 +0300

    +Doxyfile

--- classes/Base/Element.hxx
+++ classes/Base/Element.hxx
@@ -25,54 +25,54 @@ class Element
 
 private:
 
-    //- Pointer to the child
+    //! Pointer to the child of this Element object
     ElementPtr child_;
 
-    //- Pointer to the css class (if applicable)
+    //! Pointer to the css class (if applicable)
     CSSClassPtr css_;
 
-    //- Sets the pointer to new child
+    //! Sets the pointer to new child
     void set_child(ElementPtr& new_child);
 
-    //- Sets the CSS Class for this element
+    //! Sets the CSS Class for this element
     void set_css_class(const CSSClass& css);
 
-    //-
+    //! this operator adds child element to the Element el
     friend Element& operator ^ (Element& el, const Element& child);
 
-    //-
+    //! this operator applies CSSClass stored in the reference css
     friend CSSClassPtr& operator % (Element& el, const CSSClass& css);
 
-    //-
+    //! this operator applies CSSClass stored in the pointer cssptr
     friend CSSClassPtr& operator % (Element& el, CSSClassPtr& cssptr);
 
 public:
 
-    //- Default constructor
+    //! Default constructor
     Element();
 
-    //- Copy constructor
+    //! Copy constructor
     Element(const Element&);
 
-    //- Destructor
+    //! Destructor
     ~Element();
 
-    //-
+    //!
     virtual std::ostream& print(std::ostream& ostr) const;
 
-    //- creates clone of itself
+    //! creates clone of itself
     virtual ElementPtr clone() const;
 
-    //- const-ref access to the child pointr
+    //! const-ref access to the child pointr
     const ElementPtr& child() const;
 
-    //- gives const-ref access to the style class
+    //! gives const-ref access to the style class
     const CSSClassPtr& css() const;
 
 };
 
 
-//- ostream output operator
+//! ostream output operator
 std::ostream& operator << (std::ostream& ostr, const Element& elem);
 
 //- set nested element operator
